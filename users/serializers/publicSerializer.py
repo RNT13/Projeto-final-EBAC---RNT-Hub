8045c5fc9ec5bbc5a59c4ed948b8fa46679911af
RNT13 <renatornt13@gmail.com>
2025-12-22 13:59:55 -0300

    Update: - Adição de is_following no user

--- users/serializers/publicSerializer.py
+++ users/serializers/publicSerializer.py
@@ -1,10 +1,14 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from follows.models import Follow
+
 User = get_user_model()
 
 
 class UserPublicSerializer(serializers.ModelSerializer):
+    is_following = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = User
         fields = [
@@ -13,4 +17,13 @@ class UserPublicSerializer(serializers.ModelSerializer):
             "user_tag",
             "avatar",
             "is_verified",
+            "is_following",
         ]
+
+    def get_is_following(self, obj):
+        request = self.context.get("request")
+
+        if not request or request.user.is_anonymous:
+            return False
+
+        return Follow.objects.filter(follower=request.user, following=obj).exists()
