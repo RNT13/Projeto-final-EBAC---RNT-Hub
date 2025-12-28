622f2b4aa24e65566514b5023eda4be32d38d899
RNT13 <renatornt13@gmail.com>
2025-12-27 23:14:44 -0300

    Fix: - Correção no post content

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts")
-    content = models.TextField()
+    content = models.TextField(blank=True, null=True)
     image = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
